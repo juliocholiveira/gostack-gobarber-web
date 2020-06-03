c0b048d54fdde8e6473f3d70dbb331df1f5a165c
Júlio César <juliocholiveira@gmail.com>
2020-06-03 09:01:06 -0300

    altera componentes para utilizar a biblioteca unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
@@ -7,31 +8,37 @@ import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logo} alt="" />
-      <form>
-        <h1>Faça seu Cadastro</h1>
-        <Input name="name" icon={FiUser} type="text" placeholder="Name" />
-        <Input name="e-mail" icon={FiMail} type="text" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit" value="Cadastrar">
-          Cadastrar
-        </Button>
-      </form>
-      <a href="login">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" icon={FiUser} type="text" placeholder="Name" />
+          <Input name="e-mail" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit" value="Cadastrar">
+            Cadastrar
+          </Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
