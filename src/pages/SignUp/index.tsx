71b6c2443cf661fa5c9c6d64a4047158e0642f9c
Júlio César <juliocholiveira@gmail.com>
2020-06-03 06:57:13 -0300

    cria a página de cadastro

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logo} alt="" />
+      <form>
+        <h1>Faça seu Cadastro</h1>
+        <Input name="name" icon={FiUser} type="text" placeholder="Name" />
+        <Input name="e-mail" icon={FiMail} type="text" placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit" value="Cadastrar">
+          Cadastrar
+        </Button>
+      </form>
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
