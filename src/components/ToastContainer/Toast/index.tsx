df3453fbeb357abca1c4db5e838bfb6b2fcc9d6f
Júlio César <juliocholiveira@gmail.com>
2020-06-18 07:08:01 -0300

    adiciona funcionalidade de fechar o Toast após 3 segundos

---
+++ src/components/ToastContainer/Toast/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
+} from 'react-icons/fi';
+
+import { ToastMessage, useToast } from '../../../hooks/ToastContext';
+
+import { Container } from './styles';
+
+interface ToastProps {
+  message: ToastMessage;
+}
+
+const Toast: React.FC<ToastProps> = ({ message }) => {
+  const icons = {
+    info: <FiInfo size={18} />,
+    error: <FiAlertCircle size={18} />,
+    success: <FiCheckCircle size={18} />,
+  };
+
+  const { remToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      remToast(message.id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message.id, remToast]);
+
+  return (
+    <Container type={message.type}>
+      {icons[message.type || 'info']}
+      <div>
+        <span>{message.title}</span>
+        {message.description && <p>{message.description}</p>}
+      </div>
+      <button onClick={() => remToast(message.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
+};
+
+export default Toast;
--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { ToastMessage, useToast } from '../../hooks/ToastContext';
+import Toast from './Toast';
 
-import { Container, Toast } from './syles';
+import { ToastMessage } from '../../hooks/ToastContext';
+
+import { Container } from './syles';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { remToast } = useToast();
-
   return (
     <Container>
       {messages.map((message) => (
-        <Toast key={message.id}>
-          <FiAlertCircle size={20} />
-          <div>
-            <span>{message.title}</span>
-            {message.description && <p>{message.description}</p>}
-          </div>
-          <button onClick={() => remToast(message.id)} type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
